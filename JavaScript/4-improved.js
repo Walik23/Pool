de511cac0ad095fe33464f928f372f31a4c2f973
Walik23 <pruxodko2005@gmail.com>
2025-06-12 13:33:04 +0300

    task2: Rewrite 4-improved.js to class syntax

--- JavaScript/4-improved.js
+++ JavaScript/4-improved.js
@@ -1,17 +1,21 @@
 'use strict';
 
-const poolify = (factory, { size, max }) => {
-  const instances = new Array(size).fill(null).map(factory);
-
-  const acquire = () => instances.pop() || factory();
-
-  const release = (instance) => {
-    if (instances.length < max) {
-      instances.push(instance);
-    }
+class ObjectPool {
+  constructor (factory, { size, max }) {
+    this.factory = factory;
+    this.max = max;
+    this.instances = new Array(size).fill(null).map(() => this.factory());
   };
 
-  return { acquire, release };
+  acquire () {
+    return this.instances.pop() || this.factory();
+  };
+
+  release (instance) {
+    if (this.instances.length < this.max) {
+      this.instances.push(instance);
+    }
+  };
 };
 
 // Usage
@@ -21,7 +25,7 @@ const createBuffer = (size) => new Uint8Array(size);
 const FILE_BUFFER_SIZE = 4096;
 const createFileBuffer = () => createBuffer(FILE_BUFFER_SIZE);
 
-const pool = poolify(createFileBuffer, { size: 10, max: 15 });
+const pool = new ObjectPool(createFileBuffer, { size: 10, max: 15 });
 const instance = pool.acquire();
 console.log({ instance });
 pool.release(instance);
