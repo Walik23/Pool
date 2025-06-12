973b7cb2d2ccb2d4920df0477e2707657c70dd6d
Walik23 <pruxodko2005@gmail.com>
2025-06-12 13:14:57 +0300

    task1: optimize 4-bad.js

--- JavaScript/4-bad.js
+++ JavaScript/4-bad.js
@@ -1,32 +1,29 @@
 'use strict';
 
-// TODO: Refactor to respect SoC principle and
-// optimize JavaScript for V8
+const poolify = (factory, size, max) => {
+  const instances = new Array(size).fill(null).map(factory);
 
-const poolify = (factory, options, size, max) => {
-  const instances = []; // Preallocate array
-  for (let i = 0; i < size; i++) { // Use Array methods instead of loop
-    const instance = factory(...options); // Avoid array destructuring
-    instances.push(instance);
-  }
+  const acquire = () => instances.pop() || factory();
 
-  return (instance) => { // Respect SoC and SOLID/SRP
-    if (instance) { // Avoid if-statement
-      if (instances.length < max) {
-        instances.push(instance);
-      }
+  const release = (instance) => {
+    if (instances.length < max) {
+      instances.push(instance);
     }
-    instance = instances.pop(); // Do not reassign incoming parameters
-    if (!instance) instance = factory(...options);
-    return instance;
   };
+
+  return { acquire, release };
 };
 
 // Usage
 
 const createBuffer = (size) => new Uint8Array(size);
-const pool = poolify(createBuffer, [4096], 10, 15);
 
-const instance = pool();
+const bufferSize = 4096;
+const createFileBuffer = () => createBuffer(bufferSize);
+
+const size = 10;
+const max = 15;
+const pool = poolify(createFileBuffer, size, max);
+const instance = pool.acquire();
 console.log({ instance });
-pool(instance);
+pool.release(instance);
